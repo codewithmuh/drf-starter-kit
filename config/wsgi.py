051f40c020b36a7f6e58050a0bfdb91b10318ade
Muhammad Rashid <rashiddaha80@gmail.com>
2024-02-16 14:54:43 +0500

    added config structure

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-from django.config.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,9 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
     try:
-        from django.config.management import execute_from_command_line
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
